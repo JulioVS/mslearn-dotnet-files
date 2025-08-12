5f050602868136e3826fe33f9081687b7b03515c
JulioVS <julioer@me.com>
2025-08-12 09:42:29 -0300

    Upgrade code with path handling

--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
-﻿var salesFiles = FindFiles("stores");
-    
+﻿var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
+
 foreach (var file in salesFiles)
 {
-    Console.WriteLine(file);
+  Console.WriteLine(file);
 }
 
 IEnumerable<string> FindFiles(string folderName)
 {
-    List<string> salesFiles = new List<string>();
+  List<string> salesFiles = new List<string>();
 
-    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+  var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
 
-    foreach (var file in foundFiles)
+  foreach (var file in foundFiles)
+  {
+    var extension = Path.GetExtension(file);
+    if (extension == ".json")
     {
-        // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
-        {
-            salesFiles.Add(file);
-        }
+      salesFiles.Add(file);
     }
+  }
 
-    return salesFiles;
+  return salesFiles;
 }
